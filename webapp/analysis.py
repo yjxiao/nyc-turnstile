d09ea1d95c37f03336302893135561c0f7dd1aae
Yijun Xiao <ryjxiao@nyu.edu>
2015-09-29 21:15:59 -0400

    final commit

--- webapp/analysis.py
+++ webapp/analysis.py
@@ -1,3 +1,50 @@
 from data_loader import TurnstileDataLoader
+from datetime import datetime
 
-test = TurnstileDataLoader()
+def get_valid_readings(start_df, end_df):
+    setA = set()
+    setB = set()
+    for _, row in start_df.iterrows():
+        setA.add(row["TURNSTILE_ID"])
+    for _, row in end_df.iterrows():
+        setB.add(row["TURNSTILE_ID"])
+    valid_set = setA.intersection(setB)
+    bad_set = set()
+    for tid in valid_set:
+        rowA = start_df[start_df["TURNSTILE_ID"] == tid]
+        rowB = end_df[end_df["TURNSTILE_ID"] == tid]
+        if (rowA.ENTRIES.sum() > rowB.ENTRIES.sum()) |\
+           (rowA.EXITS.sum() > rowB.EXITS.sum()):
+            bad_set.add(tid)
+    valid_set -= bad_set
+    maskA = start_df.apply(lambda x: x["TURNSTILE_ID"] in valid_set, axis=1)
+    maskB = end_df.apply(lambda x: x["TURNSTILE_ID"] in valid_set, axis=1)
+    return maskA, maskB
+
+def query_stats(startdate, enddate):
+    dataloader = TurnstileDataLoader()
+    start = datetime.strptime(startdate, "%Y-%m-%d")
+    end = datetime.strptime(enddate, "%Y-%m-%d")
+    df = dataloader.retrieve(start, end)
+    df = df[df.DESC == "REGULAR"]
+    start_count = df.ix[start][["TURNSTILE_ID", "STATION", "ENTRIES", "EXITS"]]
+    end_count = df.ix[end][["TURNSTILE_ID", "STATION", "ENTRIES", "EXITS"]]
+    maskA, maskB = get_valid_readings(start_count, end_count)
+    station_countA = start_count[maskA].groupby("STATION").sum()
+    station_countB = end_count[maskB].groupby("STATION").sum()
+    station_traffic = station_countB - station_countA
+    station_traffic["TOTAL"] = station_traffic.ENTRIES + station_traffic.EXITS
+    station_traffic.sort("TOTAL", ascending=False, inplace=True)
+    total_traffic = station_traffic.TOTAL.sum()
+    results = {}
+    results["start"] = startdate
+    results["end"] = enddate
+    results["total"] = "{0:,}".format(total_traffic)
+    top10 = station_traffic[:10]
+    records = []
+    for idx in top10.index:
+        entries = "{0:,}".format(top10.ENTRIES.ix[idx].sum())
+        exits = "{0:,}".format(top10.EXITS.ix[idx].sum())
+        records.append({"station":idx, "entries":entries, "exits":exits})
+    results["records"] = records
+    return results
--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os, csv
 import pandas as pd
+from analysis import query_stats
 
 app = Flask(__name__)
 app.config.update(dict(DEBUG=True))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    result = query_stats("2013-08-01", "2013-08-02")
+    return render_template('index.html', stats=result)
 
-@app.route('/', methods=['POST'])
+@app.route('/stats', methods=['POST'])
 def home_post():
     startdate = request.form['startdate']
     enddate = request.form['enddate']
-    return "{}".format(type(startdate))
+    result = query_stats(startdate, enddate)
+    return render_template('index.html', stats=result)
 
 if __name__ == '__main__':
     app.run()
